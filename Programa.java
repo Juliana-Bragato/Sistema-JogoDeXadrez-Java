8e662ae6cc32bb43693f47bb6ef210b1107ada76
Juliana Bragato <july.plis@gmail.com>
2023-09-28 12:24:13 -0300

    First class: Position.

--- Programa.java
+++ Programa.java
@@ -1,6 +1,11 @@
+import tabuleiro.Position;
+
 public class Programa {
 
     public static void main(String[] args) {
-        
+
+        Position pos  = new Position(3, 5);
+        System.out.println(pos);
+    
     }
 }
