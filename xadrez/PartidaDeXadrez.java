c821c19b5ef40de49070e59050844a488847afaa
Juliana Bragato <july.plis@gmail.com>
2023-09-28 22:42:18 -0300

    Little Improvement in Board Printing.

--- xadrez/PartidaDeXadrez.java
+++ xadrez/PartidaDeXadrez.java
@@ -28,8 +28,18 @@ public class PartidaDeXadrez {
     }
 
     private void initialSetup() {
-        coloqueNovaPecas('b', 6, new Torre(tabuleiro, Cor.BRANCO));
-        coloqueNovaPecas('e', 8, new Rei(tabuleiro, Cor.PRETO));
-        coloqueNovaPecas('e', 1, new Rei(tabuleiro, Cor.BRANCO));
+        coloqueNovaPecas('c', 1, new Torre(tabuleiro, Cor.BRANCO));
+        coloqueNovaPecas('c', 2, new Torre(tabuleiro, Cor.BRANCO));
+        coloqueNovaPecas('d', 2, new Torre(tabuleiro, Cor.BRANCO));
+        coloqueNovaPecas('e', 2, new Torre(tabuleiro, Cor.BRANCO));
+        coloqueNovaPecas('e', 1, new Torre(tabuleiro, Cor.BRANCO));
+        coloqueNovaPecas('d', 1, new Rei(tabuleiro, Cor.BRANCO));
+
+        coloqueNovaPecas('c', 7, new Torre(tabuleiro, Cor.PRETO));
+        coloqueNovaPecas('c', 8, new Torre(tabuleiro, Cor.PRETO));
+        coloqueNovaPecas('d', 7, new Torre(tabuleiro, Cor.PRETO));
+        coloqueNovaPecas('e', 7, new Torre(tabuleiro, Cor.PRETO));
+        coloqueNovaPecas('e', 8, new Torre(tabuleiro, Cor.PRETO));
+        coloqueNovaPecas('d', 8, new Rei(tabuleiro, Cor.PRETO));
     }
 }
